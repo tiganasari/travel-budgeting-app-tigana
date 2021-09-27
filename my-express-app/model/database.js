5ea47f093086f405fa78fb4d81de7ad0d18c416d
Tigana-Sari <tigana.sari@gmail.com>
2021-09-27 21:43:15 +0100

    Set up database SQL

--- my-express-app/model/database.js
+++ my-express-app/model/database.js
@@ -10,7 +10,7 @@ const con = mysql.createConnection({
   host: DB_HOST || "127.0.0.1",
   user: DB_USER || "root",
   password: DB_PASS,
-  database: DB_NAME || "todos",
+  database: DB_NAME || "expenses",
   multipleStatements: true
 });
 
@@ -18,7 +18,8 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 
-  let sql = "DROP TABLE if exists items; CREATE TABLE items(id INT NOT NULL AUTO_INCREMENT, text VARCHAR(40) not null, complete BOOLEAN, PRIMARY KEY (id));";
+  let sql = "DROP TABLE IF EXISTS 'wallet'; CREATE TABLE `wallet` (`id` int NOT NULL AUTO_INCREMENT,`city` char NOT NULL,	`currency` char NOT NULL,	`sum` int NOT NULL,	`sum-native-currency` int NOT NULL,	`user.id` int NOT NULL, PRIMARY KEY (`id`));DROP TABLE IF EXISTS 'Expenses';CREATE TABLE `Expenses` (`id` intNOT NULL AUTO_INCREMENT,	`category` char NOT NULL,	`amount` int NOT NULL`amount-native-currency` int NOT NULL,`date` char NOT NULL,	`notes` char NOT NULL,	`wallet.id` int NOT NULL,PRIMARY KEY (`id`);DROP TABLE IF EXISTS 'User';CREATE TABLE `User` (`id` int NOT NULL AUTO_INCREMENT,`name` char NOT NULL,	`native-currency` char NOT NULL,PRIMARY KEY (`id`));";
+  
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table creation `items` was successful!");
--- my-express-app/model/helper.js
+++ my-express-app/model/helper.js
@@ -16,7 +16,7 @@ module.exports = async function db(query) {
       host: DB_HOST || "127.0.0.1",
       user: DB_USER || "root",
       password: DB_PASS,
-      database: DB_NAME || "database",
+      database: DB_NAME || "expenses",
       multipleStatements: true
     });
 
