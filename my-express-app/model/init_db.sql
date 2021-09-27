216d7b5b4558507015f8cf7d570b06f7b01e79d3
Tigana-Sari <tigana.sari@gmail.com>
2021-09-27 22:14:02 +0100

    Set up tables

--- my-express-app/model/init_db.sql
+++ my-express-app/model/init_db.sql
@@ -3,10 +3,10 @@ DROP TABLE IF EXISTS wallet;
 
 CREATE TABLE `wallet` (
 	`id` int NOT NULL AUTO_INCREMENT,
-	`city` char NOT NULL,
-	`currency` char NOT NULL,
+	`city` char(40) NOT NULL,
+	`currency` char(40) NOT NULL,
 	`sum` int NOT NULL,
-	`sum-native-currency` int NOT NULL,
+	`sum_native_currency` int NOT NULL,
 	`user.id` int NOT NULL,
 	PRIMARY KEY (`id`)
 );
@@ -14,11 +14,11 @@ DROP TABLE IF EXISTS Expenses;
 
 CREATE TABLE `Expenses` (
 	`id` int NOT NULL AUTO_INCREMENT,
-	`category` char NOT NULL,
+	`category` char(40) NOT NULL,
 	`amount` int NOT NULL,
-	`amount-native-currency` int NOT NULL,
-	`date` char NOT NULL,
-	`notes` char NOT NULL,
+	`sum_native_currency` int NOT NULL,
+	`date` char(40)NOT NULL,
+	`notes` char(40) NOT NULL,
 	`wallet.id` int NOT NULL,
 	PRIMARY KEY (`id`)
 );
@@ -27,8 +27,8 @@ DROP TABLE IF EXISTS User;
 
 CREATE TABLE `User` (
 	`id` int NOT NULL AUTO_INCREMENT,
-	`name` char NOT NULL,
-	`native-currency` char NOT NULL,
+	`name` char (40) NOT NULL,
+	`native_currency` char(40) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
