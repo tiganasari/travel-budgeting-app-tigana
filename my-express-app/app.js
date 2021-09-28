21d60e9c6272fdfcd4cfba4fa96f42fcba4b3cdd
Tigana-Sari <tigana.sari@gmail.com>
2021-09-28 19:29:30 +0100

    created routers for expenses

--- my-express-app/app.js
+++ my-express-app/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var walletsRouter = require('./routes/wallets');
+var expensesRouter = require('./routes/expenses');
 
 var app = express();
 
@@ -18,5 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/wallets', walletsRouter);
+app.use('/expenses', expensesRouter);
 
 module.exports = app;
---
+++ my-express-app/routes/expenses.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var router = express.Router();
+const db = require("../model/helper");
+
+function getAllExpenses(req, res) {
+  // Send back the full list of items
+  db("SELECT * FROM expenses ORDER BY id ASC;")
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
+}
+
+//display all wallets
+router.get("/", (req, res) => {
+  // Send back the full list of items
+  db("SELECT * FROM expenses ORDER BY id ASC")
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+//display expense by ID 
+router.get("/:expense_id", (req, res) => {
+  db(`SELECT * FROM expenses WHERE id=${req.params.expense_id}`)
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+//create a new expenses
+router.post("/", (req, res) => {
+  let {category, amount, amount_native_currency, date, notes, wallet_id} = req.body;
+  db(
+    `INSERT INTO expenses (category, amount, amount_native_currency, date, notes, wallet_id) VALUES ('${category}', ${amount}, ${amount_native_currency},'${date}', '${notes}', ${wallet_id})`
+  )
+    .then(results => {
+      // res.send({ message: "done!" });
+      getAllExpenses(req, res);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+
+//delete an expense
+router.delete("/:expense_id", (req, res) => {
+  db(`DELETE FROM expenses WHERE id=${req.params.expense_id}`)
+    .then(results => {
+      getAllExpenses(req, res);
+    })
+    .catch(err => res.status(500).send({ err: "not found" }));
+});
+
+module.exports = router;
