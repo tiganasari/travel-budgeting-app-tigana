8244f5d2e00ef5272b933d38957956b466f8a134
Tigana-Sari <tigana.sari@gmail.com>
2021-09-28 20:58:20 +0100

    display fetch results on App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,10 @@ import React, { useEffect, useState} from "react";
 function App() {
 
 const [wallets, setWallets] = useState([]); 
+const [expenses, setExpenses] = useState([]);
 
 
-const getUsers = () => {
+const getWallets = () => {
   // console.log('hi')
     fetch("/wallets")
       .then((response) => response.json())
@@ -20,16 +21,44 @@ const getUsers = () => {
       });
   };
 
+  const getExpenses = () => {
+  // console.log('hi')
+    fetch("/expenses")
+      .then((response) => response.json())
+      .then(json => {
+        // upon success, update tasks
+        console.log(json);
+        setExpenses(json);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
 
 useEffect(() => {
     // console.log("goodbye")
-    getUsers();
+    getWallets();
+    getExpenses();
   }, []);
  
 
   return (
     <div className="App">
       <h1> TRAVEL EXPENSE TRACKER APP</h1>
+      <h2>Available wallets</h2>
+      <ul>
+        {wallets.map((i) => 
+        <li id={i.id}> {i.city} </li>)}
+      </ul>
+      <h2>Transactions</h2>
+      <ul>
+        {expenses.map((i) => 
+        <li id={i.id}> {i.category} £{i.amount}</li>)}
+      </ul>
+
+        <h2>Detailed Transaction</h2>
+
     </div>
   );
 }
