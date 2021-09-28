3c98a7efc796cc465c527895f6f340b6fcd43754
Tigana-Sari <tigana.sari@gmail.com>
2021-09-28 10:42:52 +0100

    Update init_db.sql

--- my-express-app/model/init_db.sql
+++ my-express-app/model/init_db.sql
@@ -5,21 +5,21 @@ CREATE TABLE `wallet` (
 	`id` int NOT NULL AUTO_INCREMENT,
 	`city` char(40) NOT NULL,
 	`currency` char(40) NOT NULL,
-	`sum` int NOT NULL,
-	`sum_native_currency` int NOT NULL,
-	`user.id` int NOT NULL,
+	`sum` int,
+	`sum_native_currency` int,
+	`user_id` int NOT NULL,
 	PRIMARY KEY (`id`)
 );
 DROP TABLE IF EXISTS Expenses;
 
 CREATE TABLE `Expenses` (
 	`id` int NOT NULL AUTO_INCREMENT,
-	`category` char(40) NOT NULL,
+	`category` char(40),
 	`amount` int NOT NULL,
-	`sum_native_currency` int NOT NULL,
-	`date` char(40)NOT NULL,
-	`notes` char(40) NOT NULL,
-	`wallet.id` int NOT NULL,
+	`sum_native_currency` int,
+	`date` char(40),
+	`notes` char(40),
+	`wallet_id` int NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
