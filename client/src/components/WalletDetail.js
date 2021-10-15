434caf20d3abb2e61d75e418e6d301695ccdd1ed
Tigana-Sari <tigana.sari@gmail.com>
2021-10-15 22:53:26 +0100

    Added delete transaction functionality with bugs still to fixed)

--- client/src/components/WalletDetail.js
+++ client/src/components/WalletDetail.js
@@ -63,11 +63,24 @@ const WalletDetail = ({expenses, cityId, cityName , currencyName, nativeCurrency
 
     }
 
-    const onSelectTrans = () =>{
+    let history = useHistory();
 
+    const onSelectTrans = (id) =>{
+      console.log(id);
+      fetch(`/expenses/${id}`, {
+      method: "DELETE"
+    })
+    .then((response) => response.json())
+      .then(json => {
+        console.log(json);
+        alert("Transaction deleted!");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
     }
 
-    let history = useHistory();
+    
 
     const onSelectDelete = (id) => {
       console.log(id);
@@ -122,7 +135,7 @@ const WalletDetail = ({expenses, cityId, cityName , currencyName, nativeCurrency
          <ul>
         {result.map((i) => 
         <li className="transaction" key={i.id} onClick={() => onSelectItem(i.id)}>
-          <button className="button-list" key={id} onClick={() => onSelectTrans()}> <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-trash delete-icon trans-icon" viewBox="0 0 16 16">
+          <button className="button-list" key={i.id} onClick={() => onSelectTrans(i.id)}> <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-trash delete-icon trans-icon" viewBox="0 0 16 16">
         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
          </svg></button> 
