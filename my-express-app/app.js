08ea3a9f082e08a23f7b48b7e4f211ab2e84d232
Tigana Sari <tiganasari@Portas-MB15.broadband>
2021-11-08 18:45:01 +0000

    Heroku deployment

--- my-express-app/app.js
+++ my-express-app/app.js
@@ -15,6 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// Location of static assets
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -23,6 +25,11 @@ app.use('/expenses', expensesRouter);
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "../client/build/index.html"));
+});
+
+// Respond with index.html for unmatched routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build', 'index.html'));
 });
 
 module.exports = app;
